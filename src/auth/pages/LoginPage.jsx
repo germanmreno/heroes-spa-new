6d3a2623402c65283053c3a39c346303651c0ae6
German Moreno <grmmrno@gmail.com>
2024-08-20 15:37:54 -0400

    Recordar última navegación

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -7,9 +7,10 @@ export const LoginPage = () => {
     const navigate = useNavigate();
 
     const onLogin = () => {
+        const lastPath = localStorage.getItem('lastPath' || '/')
         login('Germán');
 
-        navigate("/", {
+        navigate(lastPath, {
             replace: true
         })
     }
